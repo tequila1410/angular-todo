c9166589fa89e542962fd45ec5a3137289fa3d05
VladislavZubko <vladyslav.zubko@nure.ua>
2019-01-29 00:16:20 +0200

    change task array structure

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -11,20 +11,61 @@ export class ListComponent implements OnInit {
    * Array of tasks
    * @type {string[]}
    */
-  tasks: string[] = ['Убрать в комнате', 'Посрать', 'Поспать'];
+  tasks = [
+    {id: 1, name: 'Sock', done: true},
+    {id: 2, name: 'Sleep', done: false}
+  ];
 
   taskValue: any;
 
   @ViewChild('newTaskInput') newTaskInput;
 
-  constructor() { }
+  constructor() {
+    this.tasks = JSON.parse(localStorage.getItem('tasks'));
+    if (!this.tasks) this.tasks = [];
+  }
 
   ngOnInit() {
   }
 
-  addNewTask() {
-    this.tasks.push(this.taskValue);
-    this.taskValue = null;
+  /**
+   * Add new task
+   */
+  addNewTask(): void {
+    let newId = 0;
+    if (this.tasks.length > 0) {
+      newId = this.tasks[this.tasks.length - 1].id + 1;
+    }
+
+    this.tasks.push({
+      id: newId,
+      done: false,
+      name: this.taskValue
+    });
+    this.taskValue = '';
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+  /**
+   * Toggle task status
+   * @param {number} taskId
+   */
+  toggleCheckTask(taskId: number): void {
+    this.tasks.map(task => {
+      if (task.id === taskId) {
+        task.done = !task.done;
+      }
+    });
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+  /**
+   * Delete task by id
+   * @param {number} taskId
+   */
+  deleteTask(taskId: number) {
+    // TODO realize task deleting
+    this.tasks = this.tasks.filter(task => task.id !== taskId);
   }
 
 }
